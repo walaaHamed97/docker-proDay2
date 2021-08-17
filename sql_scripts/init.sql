6eaa98ccde6f19bd48d2cd3773806b6eb213c2a5
root <root@ip-172-31-30-185.us-east-2.compute.internal>
2021-08-17 19:24:33 +0000

    Added my project

---
+++ sql_scripts/init.sql
@@ -0,0 +1,9 @@
+create table dojo_courses (
+	course text,
+	description text
+);
+
+insert into dojo_courses(course, description) values ('DevOps', 'Learn DevOps and get six figure salary');
+insert into dojo_courses(course, description) values ('Cyber Security', 'Secure your cloud infrastructure');
+insert into dojo_courses(course, description) values ('Digital Marketing', 'Learn how to do digital marketing');
+insert into dojo_courses(course, description) values ('ARVRs', 'Make AR VR games');
